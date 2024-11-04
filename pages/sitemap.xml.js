0999970c356e0973344e10a54e5c008a14b46dd0
Diego Gallardo <79814685+digallardox@users.noreply.github.com>
2024-11-04 23:44:45 +0000

    updating sitemap path

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -5,5 +5,6 @@ export default function Sitemap() {}
 export function getServerSideProps(ctx) {
   return getSitemapProps(ctx, {
     frontendUrl: process.env.FRONTEND_URL,
+    sitemapIndexPath: "/sitemap_index.xml"
   });
 }
