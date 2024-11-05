40a2b097e7b47e539d6c01990d6146db7219219e
Diego Gallardo <79814685+digallardox@users.noreply.github.com>
2024-11-05 02:15:02 +0000

    updating GTML in body

--- pages/_document.js
+++ pages/_document.js
@@ -6,12 +6,8 @@ export default function Document() {
       <Head>{/* Head content can be added here if needed */}</Head>
       <body>
         <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-KXTXW4CQ"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          ></iframe>
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K5GL3H8W" height="0" width="0" style="display:none;visibility:hidden">
+          </iframe>
         </noscript>
         <Main />
         <NextScript />
