215ddf8928f94e04f29e4d7bcc7591969d5c5468
Diego Gallardo <79814685+digallardox@users.noreply.github.com>
2024-11-05 02:32:54 +0000

    converting GTM to use <GoogleTagManager> componenet provided by Next.js

--- pages/_app.js
+++ pages/_app.js
@@ -4,29 +4,15 @@ import { useRouter } from 'next/router';
 import { FaustProvider } from '@faustwp/core';
 import '@faustwp/core/dist/css/toolbar.css';
 import '../styles/global.scss';
-import Script from "next/script";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 export default function MyApp({ Component, pageProps }) {
   const router = useRouter();
 
   return (
     <FaustProvider pageProps={pageProps}>
-      <Script
-        id="gtm-script"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            <!-- Google Tag Manager -->
-            <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-K5GL3H8W');</script>
-            <!-- End Google Tag Manager -->
-          `,
-        }}
-      />
       <Component {...pageProps} key={router.asPath} />
+      <GoogleTagManager gtmId="GTM-K5GL3H8W" />
     </FaustProvider>
   );
 }
