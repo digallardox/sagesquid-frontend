10dc28d3876f34d8e5b5f3dc489f784eaac9649e
Diego Gallardo <79814685+digallardox@users.noreply.github.com>
2024-11-05 02:23:07 +0000

    issue with GTM in body. Update to fix syntax

--- pages/_document.js
+++ pages/_document.js
@@ -3,12 +3,17 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>{/* Head content can be added here if needed */}</Head>
       <body>
+        {/* Google Tag Manager (noscript) */}
         <noscript>
-          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K5GL3H8W" height="0" width="0" style="display:none;visibility:hidden">
-          </iframe>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-K5GL3H8W"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
         </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <Main />
         <NextScript />
       </body>
